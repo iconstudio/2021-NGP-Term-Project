e3046dfc94f18d62240c0804f0c2790df1a4c7c3
Iconer <yoyofa2@hotmail.com>
2021-11-13 15:06:27 +0900

    게임 초기화 동작 바꾸기
    - 사실 추가적인 스레드는 필요 없다.
    - 플레이어 캐릭터 생성하기.

--- src/Battleroyale Server/BattleroyaleServer.cpp
+++ src/Battleroyale Server/BattleroyaleServer.cpp
@@ -56,7 +56,19 @@ DWORD WINAPI CommunicateProcess(LPVOID arg) {
 				// 방장의 게임 시작 메시지
 				if (packet->type == PACKETS::CLIENT_GAME_START) {
 					// 게임 초기화
-					SetEvent(framework.event_game_start);
+					//SetEvent(framework.event_game_start);
+
+					shuffle(framework.players.begin(), framework.players.end(), server_randomizer);
+
+					auto sz = framework.players.size();
+					for (int i = 0; i < sz; ++i) {
+						auto player = framework.players.at(i);
+						auto places = framework.PLAYER_SPAWN_PLACES[i];
+						player->player_character = framework.Instantiate<CCharacter>(places[0], places[1]);
+					}
+
+					SetEvent(framework.event_receives);
+					framework.SetStatus(GAME);
 				}
 			}
 			break;
@@ -64,8 +76,10 @@ DWORD WINAPI CommunicateProcess(LPVOID arg) {
 			case GAME:
 			{
 				// 꾸준한 통신
+				while (true) {
+					WaitForSingleObject(framework.event_receives, FRAME_TIME);
 
-
+				}
 			}
 			break;
 
@@ -92,11 +106,14 @@ DWORD WINAPI CommunicateProcess(LPVOID arg) {
 	return 0;
 }
 
+/*
+		TODO: 나중에 삭제하기
+*/
 DWORD __stdcall GameInitializeProcess(LPVOID arg) {
 	while (true) {
 		WaitForSingleObject(framework.event_game_start, INFINITE);
 
-		shuffle(framework.players. begin(), framework.players.end(), server_randomizer);
+		shuffle(framework.players.begin(), framework.players.end(), server_randomizer);
 		
 		auto sz = framework.players.size();
 		for (int i = 0; i < sz; ++i) {
