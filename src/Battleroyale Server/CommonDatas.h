94c1835d815e84dfa863b6431d03adc9bb49cc21
Iconer <yoyofa2@hotmail.com>
2021-11-13 16:40:21 +0900

    SendMyMessage() 함수의 정의와 선언 분리

--- src/Battleroyale Server/CommonDatas.h
+++ src/Battleroyale Server/CommonDatas.h
@@ -50,24 +50,4 @@ struct RenderInstance {
 	double x, y, angle;
 };
 
-int SendMyMessage(
-	SOCKET sk
-	, PACKETS type
-	, int size
-	, void* data = nullptr
-) {
-	PacketMessage packet;
-	packet.type = type;
-	packet.size = size;
-
-	if (0 < size) {
-		int result = send(sk, reinterpret_cast<char*>(&packet), sizeof(packet), 0);
-		send(sk, reinterpret_cast<char*>(data), size, 0);
-
-		return result;
-	} else {
-		return send(sk, reinterpret_cast<char*>(&packet), sizeof(packet), 0);
-	}
-	
-}
-
+int SendMyMessage(SOCKET sk, PACKETS type, int size, void* data = nullptr);
