f8d0738aa26e0fc5b0aeb45125a7c339f0460c6b
Iconer <yoyofa2@hotmail.com>
2021-11-17 21:51:02 +0900

    서버의 프레임 시간을 50으로 함

--- src/Battleroyale Server/CommonDatas.h
+++ src/Battleroyale Server/CommonDatas.h
@@ -11,7 +11,7 @@ const double EWALL_DAMAGE_PER_SECOND = 1.5;
 const int PLAYERS_NUMBER_MAX = 10;
 
 // 프레임 수
-const int FRAMERATE = 60;
+const int FRAMERATE = 50;
 const double FRAME_TIME = (1.0 / FRAMERATE);
 
 enum PACKETS : int {
