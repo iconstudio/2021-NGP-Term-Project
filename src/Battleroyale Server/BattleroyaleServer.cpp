806d3ab2d1ac3c28607f6391f889ea0ec7615119
Iconer <yoyofa2@hotmail.com>
2021-11-25 16:46:42 +0900

    서버의 입력 처리

--- src/Battleroyale Server/BattleroyaleServer.cpp
+++ src/Battleroyale Server/BattleroyaleServer.cpp
@@ -84,6 +84,7 @@ DWORD WINAPI CommunicateProcess(LPVOID arg) {
 
 					// 만약 핑 메시지가 오면 데이터를 받지 않는다.
 					if (packet == PACKETS::CLIENT_KEY_INPUT) {
+						auto key_storage = new InputStream[SEND_INPUT_COUNT];
 						data_size = SEND_INPUT_COUNT * sizeof(InputStream);
 
 						result = recv(client_socket, reinterpret_cast<char*>(key_storage)
@@ -96,81 +97,79 @@ DWORD WINAPI CommunicateProcess(LPVOID arg) {
 							break;
 						}
 
+						bool check_lt = false;
+						bool check_rt = false;
+						bool check_up = false;
+						bool check_dw = false;
+						bool check_shoot = false;
+						bool check_blink = false;
 						for (int i = 0; i < SEND_INPUT_COUNT; ++i) {
 							auto button = key_storage[i];
 							auto keycode = button.code;
-							auto keystat = button.type; // NONE, PRESS, RELEASE
-
+							auto keystat = button.type;
+							
 							switch (keystat) {
 								case NONE:
 								{
-
+									if (keycode == 'A') {
+										check_lt = false;
+									} else if (keycode == 'D') {
+										check_rt = false;
+									} else if (keycode == 'W') {
+										check_up = false;
+									} else if (keycode == 'S') {
+										check_dw = false;
+									} else if (keycode == VK_SPACE) { // 특능
+										check_blink = false;
+									} else if (keycode == 'A') { // 공격
+										check_shoot = false;
+									}
 								}
 								break;
 
 								case PRESS:
 								{
-
+									if(keycode == 'A') {
+										check_lt = true;
+									} else if (keycode == 'D') {
+										check_rt = true;
+									} else if (keycode == 'W') {
+										check_up = true;
+									} else if (keycode == 'S') {
+										check_dw = true;
+									} else if (keycode == VK_SPACE) { // 특능
+										check_blink = true;
+									} else if (keycode == 'A') { // 공격
+										check_shoot = true;
+									}
 								}
 								break;
 
 								case RELEASE:
 								{
-
-								}
-								break;
-
-							}
-							switch (button) {
-								case 'W': case 'w':
-								{
-									framework.QueingPlayerAction(client_info
-																 , ACTION_TYPES::SET_HSPEED
-																 , -PLAYER_MOVE_SPEED);
-									std::cout << player_index << " - w" << std::endl;
-								}
-								break;
-
-								case 'A': case 'a':
-								{
-									framework.QueingPlayerAction(client_info
-																 , ACTION_TYPES::SET_HSPEED
-																 , PLAYER_MOVE_SPEED);
-									std::cout << player_index << " - a" << std::endl;
-								}
-								break;
-
-								case 'S': case 's':
-								{
-									framework.QueingPlayerAction(client_info
-																 , ACTION_TYPES::SET_VSPEED
-																 , -PLAYER_MOVE_SPEED);
-									std::cout << player_index << " - s" << std::endl;
-								}
-								break;
-
-								case 'D': case 'd':
-								{
-									framework.QueingPlayerAction(client_info
-																 , ACTION_TYPES::SET_VSPEED
-																 , PLAYER_MOVE_SPEED);
-									std::cout << player_index << " - d" << std::endl;
-								}
-								break;
-
-								case VK_SPACE:
-								{
-									auto cc = reinterpret_cast<CCharacter*>(client_info->player_character);
-									if (cc) {
-										framework.QueingPlayerAction(client_info
-																	 , ACTION_TYPES::SHOOT
-																	 , static_cast<int>(cc->image_angle));
+									if (keycode == 'A') {
+										check_lt = false;
+									} else if (keycode == 'D') {
+										check_rt = false;
+									} else if (keycode == 'W') {
+										check_up = false;
+									} else if (keycode == 'S') {
+										check_dw = false;
+									} else if (keycode == VK_SPACE) { // 특능
+										check_blink = false;
+									} else if (keycode == 'A') { // 공격
+										check_shoot = false;
 									}
-									std::cout << player_index << " - space" << std::endl;
 								}
 								break;
 							}
 						}
+
+						int check_horz = check_rt - check_lt; // 좌우 이동
+						int check_vert = check_dw - check_up; // 상하 이동
+
+
+
 					} // 다른 메시지는 버린다.
 
 					framework.CastProcessingGame();
