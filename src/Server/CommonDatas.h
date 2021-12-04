0a1f044c4b701ad1eb9344f9da61b1c37305f50a
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2021-12-04 17:52:49 +0900

    매크로->enum 변경

--- src/Server/CommonDatas.h
+++ src/Server/CommonDatas.h
@@ -1,16 +1,18 @@
 ﻿#pragma once
 #include "stdafx.h"
 
-#define COMMON_PORT 15000
-#define GAME_SCENE_W 1280
-#define GAME_SCENE_H 1280
-#define CLIENT_W 960
-#define CLIENT_H 540
-#define VIEW_W 320
-#define VIEW_H 240
-#define PORT_W 640
-#define PORT_H 480
-
+enum ClinetInfo
+{
+	COMMON_PORT = 15000,
+	GAME_SCENE_W = 1280,
+	GAME_SCENE_H = 1280,
+	CLIENT_W = 960,
+	CLIENT_H = 540,
+	VIEW_W = 320,
+	VIEW_H = 240,
+	PORT_W = 640,
+	PORT_H = 480
+};
 
 constexpr double METER_TO_PIXELS = 16.;
 constexpr double HOUR_TO_SECONDS = 3600.;
