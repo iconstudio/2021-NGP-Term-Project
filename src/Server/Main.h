95ee65b4f4490c09bb68e876abf653d2c05378e6
Iconer <yoyofa2@hotmail.com>
2021-12-04 15:59:23 +0900

    서버에 필요한 함수들

--- src/Server/Main.h
+++ src/Server/Main.h
@@ -1,3 +1,28 @@
 #pragma once
 #include "stdafx.h"
 
+void ProceedContinuation();
+bool CheckClientNumber();
+bool ValidateSocketMessage(int socket_state);
+void BakeRenderingInfos();
+void SendRenderingInfos(SOCKET my_socket);
+
+template<typename Ty>
+void AtomicPrint(Ty caption) {
+	//EnterCriticalSection(&print_permission);
+	cout << caption;
+	//LeaveCriticalSection(&print_permission);
+}
+
+template<typename Ty1, typename... Ty2>
+void AtomicPrint(Ty1 caption, Ty2... args) {
+	//EnterCriticalSection(&print_permission);
+	cout << caption;
+	//LeaveCriticalSection(&print_permission);
+	AtomicPrint(args...);
+}
+
+template<typename... Ty>
+void AtomicPrintLn(Ty... args) {
+	AtomicPrint(args..., "\n");
+}
