1a6efa208229981ca9b6e32295658970eb387eb1
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2021-12-05 17:09:05 +0900

    프레임워크 클래스 생성

--- src/Server/Main.cpp
+++ src/Server/Main.cpp
@@ -1,136 +1,70 @@
 ﻿#include "stdafx.h"
 #include "Framework.h"
 #include "CommonDatas.h"
-#include "ServerFramework.h"
 #include "Main.h"
-
+#include "ServerFramework.h"
 
 // 스레드 프로세스
 DWORD WINAPI ConnectProcess(LPVOID arg);
 DWORD WINAPI GameProcess(LPVOID arg);
 
-// 소켓 정보
-SOCKET my_socket;
-SOCKADDR_IN my_address;
-int my_address_size = sizeof(my_address);
-
-HANDLE event_accept; // 클라이언트 수용 신호
-HANDLE event_game_process; // 게임 처리 신호
-
-RenderInstance rendering_infos_last[RENDER_INST_COUNT];
+ServerFramework f{};
 
 int main() {
-	WSADATA wsadata;
-	if (0 != WSAStartup(MAKEWORD(2, 2), &wsadata)) {
-		ErrorAbort("WSAStartup()");
-		return false;
-	}
+	f.Initialize();
 
-	my_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (INVALID_SOCKET == my_socket) {
-		ErrorAbort("socket()");
-		return false;
-	}
-
-	BOOL option = TRUE;
-	if (SOCKET_ERROR == setsockopt(my_socket, SOL_SOCKET, SO_REUSEADDR
-		, reinterpret_cast<char*>(&option), sizeof(option))) {
-		ErrorAbort("setsockopt()");
-		return false;
-	}
-
-	ZeroMemory(&my_address, sizeof(my_address));
-	my_address.sin_family = AF_INET;
-	my_address.sin_addr.s_addr = htonl(INADDR_ANY);
-	my_address.sin_port = htons(COMMON_PORT);
-
-	if (SOCKET_ERROR == bind(my_socket, reinterpret_cast<SOCKADDR*>(&my_address), my_address_size)) {
-		ErrorAbort("bind()");
-		return false;
-	}
-
-	if (SOCKET_ERROR == listen(my_socket, CLIENT_NUMBER_MAX + 1)) {
-		ErrorAbort("listen()");
-		return false;
-	}
-
-	AtomicPrintLn("서버 시작");
-
-	players_number = 0;
-	players.reserve(CLIENT_NUMBER_MAX);
-
-	PLAYER_SPAWN_PLACES = new int* [CLIENT_NUMBER_MAX];
-
-	double dir_increment = (360.0 / CLIENT_NUMBER_MAX);
-	for (int i = 0; i < CLIENT_NUMBER_MAX; ++i) {
-		double dir = dir_increment * i;
-		int cx = static_cast<int>(WORLD_W * 0.5 + lengthdir_x(SPAWN_DISTANCE, dir));
-		int cy = static_cast<int>(WORLD_W * 0.5 + lengthdir_y(SPAWN_DISTANCE, dir));
-
-		PLAYER_SPAWN_PLACES[i] = new int[2]{ cx, cy };
-	}
-
-	event_accept = CreateEvent(NULL, FALSE, TRUE, NULL);
-	event_game_process = CreateEvent(NULL, FALSE, FALSE, NULL);
-
-	// 클라이언트 연결
 	CreateThread(NULL, 0, ConnectProcess, nullptr, 0, NULL);
 
 	Sleep(8000);
-	CreatePlayerCharacters();
-	SetEvent(event_game_process);
+	f.CreatePlayer();
+	f.SetGameProcess();
 
-	while (true) {
-		// 서버 대기
+	// 클라이언트 연결
+	while (true)
+	{
 	}
-
-	CloseHandle(event_accept);
-	CloseHandle(event_game_process);
-	closesocket(my_socket);
-
-	return 0;
 }
 
-CCharacter::CCharacter()
-	: GameInstance() {
+CCharacter::CCharacter() : GameInstance() {
 	SetRenderType(RENDER_TYPES::CHARACTER);
 	SetBoundBox(RECT{ -6, -6, 6, 6 });
 }
 
 DWORD WINAPI ConnectProcess(LPVOID arg) {
 	while (true) {
+		SOCKET listen_socket = f.GetListenSocket();
 		SOCKET client_socket;
 		SOCKADDR_IN client_address;
 		int my_addr_size = sizeof(client_address);
 
-		SetEvent(event_accept);
+		f.SetConnectProcess();
 
-		client_socket = accept(my_socket, reinterpret_cast<SOCKADDR*>(&client_address), &my_addr_size);
+		client_socket = accept(listen_socket, reinterpret_cast<SOCKADDR*>(&client_address), &my_addr_size);
 		if (INVALID_SOCKET == client_socket) {
 			ErrorDisplay("connect()");
 			continue;
 		}
 
-		BOOL option = FALSE;							//네이글 알고리즘 on/off
-		setsockopt(my_socket,						//해당 소켓
+		BOOL option = FALSE;						//네이글 알고리즘 on/off
+		setsockopt(listen_socket,					//해당 소켓
 			IPPROTO_TCP,							//소켓의 레벨
 			TCP_NODELAY,							//설정 옵션
 			reinterpret_cast<const char*>(&option),	// 옵션 포인터
 			sizeof(option));						//옵션 크기
 
-		auto client = new ClientSession(client_socket, NULL, players_number++);
+		auto client = new ClientSession(client_socket, NULL, f.GetPlayerNumber());
 
-		auto th = CreateThread(NULL, 0, GameProcess, (LPVOID)(client), 0, NULL);
+		auto th = CreateThread(NULL, 0, GameProcess, reinterpret_cast<LPVOID>(client), 0, NULL);
 		if (!th) {
 			ErrorDisplay("CreateThread()");
 			continue;
 		}
 		CloseHandle(th);
-		players.push_back(client);
+		f.AddPlayer(client);
 
-		AtomicPrintLn("클라이언트 접속: ", client_socket, ", 수: ", players_number);
+		AtomicPrintLn("클라이언트 접속: ", client_socket, ", 수: ", f.GetPlayerNumber());
 
-		WaitForSingleObject(event_accept, INFINITE);
+		WaitForSingleObject(f.GetAcceptEvent(), INFINITE);
 	}
 
 	return 0;
@@ -141,7 +75,7 @@ DWORD WINAPI GameProcess(LPVOID arg) {
 	SOCKET client_socket = client->my_socket;
 
 	while (true) {
-		WaitForSingleObject(event_game_process, INFINITE);
+		WaitForSingleObject(f.GetGameProcessEvent(), INFINITE);
 
 		PACKETS header;
 		ZeroMemory(&header, HEADER_SIZE);
@@ -227,91 +161,17 @@ DWORD WINAPI GameProcess(LPVOID arg) {
 		// 3. 게임 처리
 
 		// 4. 렌더링 정보 작성
-		BakeRenderingInfos();
+		f.CreateRenderingInfos();
 
 		// 5. 렌더링 정보 전송
-		SendRenderingInfos(client_socket);
+		f.SendRenderingInfos(client_socket);
 
 		// 6. 대기
 		Sleep(FRAME_TIME);
-		SetEvent(event_game_process);
+		f.SetGameProcess();
 	}
 
 	return 0;
-}
-
-void ClientConnect() {
-}
-
-void ClientDisconnect(int player_index) {
-}
-
-void CreatePlayerCharacters() {
-	auto sz = players.size();
-	for (int i = 0; i < sz; ++i) {
-		auto player = players.at(i);
-		int places[2] = {80, 80};//PLAYER_SPAWN_PLACES[i];
-		auto character = Instantiate<CCharacter>(places[0], places[1]);
-
-		player->player_character = character;
-		character->owner = player->player_index;
-		//SendData(player->my_socket, PACKETS::SERVER_GAME_START);
-	}
-}
-
-void ProceedContinuation() {
-
-}
-
-bool CheckClientNumber() {
-	return false;
-}
-
-bool ValidateSocketMessage(int socket_state) {
-	return false;
-}
-
-// 렌더링 정보 생성 함수
-void BakeRenderingInfos() {
-	if (!instances.empty()) {
-		AtomicPrintLn("렌더링 정보 생성\n크기: ", instances.size());
-		if (rendering_infos_last) {
-			ZeroMemory(rendering_infos_last, sizeof(rendering_infos_last));
-		}
-
-		auto CopyList = vector<GameInstance*>(instances);
-
-		// 플레이어 개체를 맨 위로
-		std::partition(CopyList.begin(), CopyList.end(), [&] (GameInstance* inst) {
-			return (strcmp(inst->GetIdentifier(), "Player") == 0);
-		});
-
-		int index = 0;
-		for (auto it = CopyList.begin(); it != CopyList.end(); ++it) {
-			auto render_infos = (*it)->GetRenderInstance();
-
-			// 인스턴스가 살아있는 경우에만 렌더링 메세지 전송
-			if (!(*it)->dead) {
-				auto dest = (rendering_infos_last + index);
-				auto src = &render_infos;
-
-				memcpy(dest, src, sizeof(RenderInstance));
-				index++;
-			}
-		}
-	} else if (rendering_infos_last) {
-		if (rendering_infos_last) {
-			ZeroMemory(rendering_infos_last, sizeof(rendering_infos_last));
-		}
-	}
-}
-
-// 렌더링 정보 전송
-void SendRenderingInfos(SOCKET client_socket) {
-	auto renderings = reinterpret_cast<char*>(rendering_infos_last);
-	auto render_size = sizeof(rendering_infos_last);
-
-	SendData(client_socket, SERVER_RENDER_INFO, renderings, render_size);
 }
 
 const char* CCharacter::GetIdentifier() const { return "Player"; }
--- src/Server/Main.h
+++ src/Server/Main.h
@@ -24,90 +24,13 @@ public:
 	~ClientSession();
 };
 
-/* 플레이어 관련 속성 */
-vector<ClientSession*> players; // 플레이어 목록
-int player_process_index; // 현재 처리 중인 플레이어의 순번 [0~client_number)
-int	players_number; // 지금 접속한 플레이어의 수
-int player_number_last; // 마지막에 추가된 플레이어의 번호
-int	player_captain; // 방장 플레이어
-int player_winner; // 승리한 플레이어
-
 /* 게임 관련 속성 */
-vector<GameInstance*> instances; // 인스턴스 목록
 normal_distribution<> random_distrubution; // 서버의 무작위 분포 범위
 default_random_engine randomizer;
 
-bool game_started;
 const int WORLD_W = 1280, WORLD_H = 1280;
 int** PLAYER_SPAWN_PLACES; // 플레이어가 맨 처음에 생성될 위치의 배열
 const int SPAWN_DISTANCE = 300; // 플레이어 생성 위치를 정할 때 사용하는 거리 값
-
-// 지정한 위치에 인스턴스를 생성한다.
-template<class _GameClass = GameInstance>
-_GameClass* Instantiate(double x = 0.0, double y = 0.0) {
-	auto result = new _GameClass();
-	result->x = x;
-	result->y = y;
-
-	instances.push_back(result);
-
-	return result;
-}
-
-// 지정한 인스턴스를 삭제한다.
-template<class _GameClass = GameInstance>
-void Kill(_GameClass* target) {
-	auto loc = find_if(instances.begin(), instances.end(), [target] (const auto& lhs) {
-		return (lhs == target);
-	});
-
-	if (loc != instances.end()) {
-		target->OnDestroy();
-		instances.erase(loc);
-	}
-}
-
-// 두 게임 인스턴스의 충돌을 검사한다.
-template<class _GameClass1, class _GameClass2>
-inline _GameClass2* CheckCollision(_GameClass1* self, _GameClass2* other) {
-	if (self && other && self != other) {
-		if (self->IsCollideWith(other))
-			return other;
-	}
-	return nullptr;
-}
-
-// 어떤 게임 인스턴스에게 충돌하는 인스턴스를, 식별자 fid를 기반으로 찾아낸다.
-template<class _GameClassTarget, class _GameClassSelf>
-_GameClassTarget* SeekCollision(_GameClassSelf* self, const char* fid) {
-	if (self && !instances.empty()) {
-		auto CopyList = vector<GameInstance*>(instances);
-
-		auto it = std::find_if(CopyList.begin(), CopyList.end(), [&] (GameInstance* inst) {
-			auto iid = inst->GetIdentifier();
-			auto id_check = strcmp(iid, fid);
-
-			return (0 == id_check);
-		});
-
-		if (it != CopyList.end()) {
-			return dynamic_cast<_GameClassTarget*>(*it);
-		}
-	}
-	return nullptr;
-}
-
-void ClientConnect();
-void ClientDisconnect(int player_index);
-
-// 정해둔 스폰 지점에 플레이어 캐릭터들을 생성한다.
-void CreatePlayerCharacters();
-
-void ProceedContinuation(); // 게임 진행 확인
-bool CheckClientNumber(); // 접속한 클라이언트 수 확인
-bool ValidateSocketMessage(int socket_state); // 받은 소켓 메시지 검증
-void BakeRenderingInfos(); // 렌더링 정보 만들기
-void SendRenderingInfos(SOCKET my_socket); // 렌더링 정보 보내기
 
 // cout으로 출력하기
 template<typename Ty>
